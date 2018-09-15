c48ab4467fc59d4df285cbdc00b0c0b331d7872c
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2018-09-15 04:17:58 -0500

    Add files via upload

---
+++ Source/SCDLT.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+ofstream GenFile;
+GenFile.open(Data);
+GenFile.close();
+//For logger file
+Traffic =  k, device, Response;
+//Calls IP
+IP = Data;
+//Calls Port
+Port = Data;
+//Sends response
+std::cout << Packet;
+return 0;
+}
+
