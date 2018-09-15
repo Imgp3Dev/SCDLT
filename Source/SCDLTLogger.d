8857d9419ffeff6f5444858fc0cbeade1268ca6c
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2018-09-15 04:17:17 -0500

    Create SCDLTLogger.d

---
+++ Source/SCDLTLogger.d
@@ -0,0 +1,56 @@
+import std.stdio
+import std.array
+import std.string
+
+extern (C++)
+/*Creating string*/
+Data[string] = Logger;
+/*Defining Data with Traffic*/
+Data = Traffic;
+Traffic = {""}
+/*Using Find function to Find integers 
+relating to the Traffic*/
+
+foreach (find; "Logging"  Traffic, %s / 1 
+GenFile) {
+
+immutable Traffic = [uint](T)(T) .. $;
+}
+/*Posting Data to The Generated File*/
+foreach (Data, %s / 1 GenFile) {
+Data.post(Genfile);
+}
+void main() {
+/*****Commands
+*******/
+foreach (Log; %s [real]%u .. $ .. 
+Traffic) {
+writeln(Genfile);
+}
+
+foreach (send; Data .. %s; GenFile, 
+search.Traffic) {
+Data.send(GenFile);
+}
+
+immutable Traffic(uint IP; Port);
+Data.search(Response);
+Data.post(Response);
+
+/*Sending response towards specific IP & 
+Port*/
+auto Response(3)[uint][string] = IP;
+
+auto Response(3)[uint][string] = Port;
+*/Sends Response as Packet*/
+Packet.post(Response);
+/*Response Relies on The strings within the 
+traffic If there are none then it won't 
+proceed*/
+if (Response .. %s)
+/*Calling all Variables*/
+foreach (Response; Data; IP; Port; Traffic; 
+Logger){
+/*Outputs the packets*/
+writeln(Packet);
+}
